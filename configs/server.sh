bda07df42b4831465831258a951f78edb1dacf38
alejo-guevara <jaguevara@gmail.com>
2023-12-12 17:23:29 -0500

    Adding Web Server

---
+++ configs/server.sh
@@ -0,0 +1,12 @@
+apk add openrc 
+sleep 1
+apk add bind 
+sleep 1
+apk add apache2
+touch /run/openrc/softlevel
+sleep 1
+/etc/rc.d/apache2 stop
+/etc/init.d/nginx stop
+/usr/sbin/httpd
+sleep 1
+rc-service named startenrc
